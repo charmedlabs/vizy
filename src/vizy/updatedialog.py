25ec9747ac04a472fb0cb19cd6e11df580d9afbb
rich legrand <rich@charmedlabs.com>
2021-10-20 14:11:46 -0500

    Fix upload dialog

--- src/vizy/updatedialog.py
+++ src/vizy/updatedialog.py
@@ -27,15 +27,19 @@ def get_latest(config, tries=3):
             e = _e
     raise e
 
-
 class UpdateDialog:
+
+    # Note, we removed drag-drop file functionality, because as the build packages
+    # became bigger (20MB), it became unreliable, so it has been removed for now.
+    # There are size limits in the Quart configuration, but this doesn't seem to 
+    # be the whole story.  
 
     def __init__(self, kapp, pmask):
         self.kapp = kapp
 
-        check_button = Kbutton(name="Check for updates at vizycam.com", id=Kritter.new_id(), spinner=True)
+        check_button = Kbutton(name="Check for updates at vizycam.com", spinner=True)
         check_response = html.Div(id=Kritter.new_id())
-        install_button = Kbutton(name="Install", id=Kritter.new_id(), spinner=True)
+        install_button = Kbutton(name="Install", spinner=True)
         @install_button.callback()
         def func():
             # Block unauthorized attempts
@@ -47,29 +51,12 @@ class UpdateDialog:
             # run because it waits, so this func will take a few more seconds to return the result.  
             return self.update_dialog.out_open(False) + install_button.out_spinner_disp(False) + self.kapp.execterm.exec(command=f"python3 {os.path.join(self.kapp.homedir, SCRIPTSDIR_NAME, INSTALL_UPDATE_SCRIPT)}", size="lg", backdrop="static", close_button=False, logfile=os.path.join(self.kapp.homedir, SCRIPTSDIR_NAME, "install.log"))
 
-        upload = dcc.Upload(id=Kritter.new_id(), children=html.Div([
-                html.Div('Drag and drop Vizy software package here.'),
-                html.Div('Or click here to select local file.'),
-            ]), style={
-                'width': '100%',
-                'height': '100px',
-                'lineHeight': '20px',
-                'padding-top': '30px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-            },
-            multiple=False
-        )
-
         update_url = dbc.Input(id=Kritter.new_id(), placeholder="Copy URL of Vizy software package here and press Install.", type="text")
-        install_button2 = Kbutton(name="Install", id=Kritter.new_id())
+        install_button2 = Kbutton(name="Install", spinner=True)
 
         update_layout = [
             html.Div("You are currently running Vizy version "+__version__+".", style={"padding-bottom": "10px"}),
-            check_button, check_response, html.Div("--OR--",style={"padding": "10px 0px 10px 0px"}),
-            html.Div(upload),
+            check_button, check_response, 
             html.Div("--OR--",style={"padding": "10px 0px 10px 0px"}), 
             html.Div([update_url, install_button2]),
         ]
@@ -88,38 +75,20 @@ class UpdateDialog:
                 return [Output(check_response.id, "children", "Error: " + str(e))] + check_button.out_spinner_disp(False)
             return [Output(check_response.id, "children", "You are running the latest version.")] + check_button.out_spinner_disp(False)
 
-        @self.kapp.callback_shared(None,
-            [Input(upload.id, 'contents')], [State(upload.id, 'filename')]
-        )
-        def func(contents, filename):
-            # Block unauthorized attempts
-            if not callback_context.client.authentication&pmask or not contents or not filename:
-                return
-            # Contents are type and contents separated by comma, so we grab 2nd item.
-            # See https://dash.plotly.com/dash-core-components/upload
-            bin_contents = base64.b64decode(contents.split(",")[1])
-            # Make temp directory 
-            install_dir = os.path.join(self.kapp.homedir, "tmp")
-            os.system("mkdir " + install_dir)
-            filename = os.path.join(install_dir, filename)
-            # Write file
-            file = open(filename, "wb")
-            file.write(bin_contents)
-            file.close()
-            # Return contents to reset so we can set again
-            return [Output(self.update_dialog.id, "is_open", False), self.kapp.execterm.exec(command=f"python3 {os.path.join(self.kapp.homedir, SCRIPTSDIR_NAME, INSTALL_UPDATE_SCRIPT)} {filename} -r", size="lg", backdrop="static", close_button=False, logfile=os.path.joinself)]
-
         @install_button2.callback([State(update_url.id, 'value')])
         def func(url):
             # Block unauthorized attempts
             if not callback_context.client.authentication&pmask or not url:
                 return
-            return [Output(self.update_dialog.id, "is_open", False), self.kapp.execterm.exec(command="python3 " + os.path.join(self.kapp.homedir, SCRIPTSDIR_NAME, INSTALL_UPDATE_SCRIPT) + " " + url + " -r", size="lg", backdrop="static", close_button=False)]
+            if not url.startswith("http://") and not url.startswith("https://"):
+                url = "http://" + url
+            self.kapp.push_mods(install_button2.out_spinner_disp(True))
+            return self.update_dialog.out_open(False) + install_button2.out_spinner_disp(False) + self.kapp.execterm.exec(command=f"python3 {os.path.join(self.kapp.homedir, SCRIPTSDIR_NAME, INSTALL_UPDATE_SCRIPT)} {url}", size="lg", backdrop="static", close_button=False, logfile=os.path.join(self.kapp.homedir, SCRIPTSDIR_NAME, "install.log"))
 
         @self.update_dialog.callback_view()
         def func(enable):
             if not enable:
-                return check_button.out_spinner_disp(False) + [Output(check_response.id, "children", None), Output(update_url.id, 'value', None), Output(upload.id, 'contents', None)]
+                return check_button.out_spinner_disp(False) + [Output(check_response.id, "children", None), Output(update_url.id, 'value', None)]
 
         self.layout = KsideMenuItem("Update", self.update_dialog, "chevron-circle-up", kapp=self.kapp)
 
