5a0c9dc69cd3fd736d2fdb28e9d5a02e94983638
rich legrand <rich@charmedlabs.com>
2022-02-11 13:54:27 -0600

    Add license header

--- src/vizy/__init__.py
+++ src/vizy/__init__.py
@@ -1,3 +1,13 @@
+#
+# This file is part of Vizy 
+#
+# All Vizy source code is provided under the terms of the
+# GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
+# Those wishing to use Vizy source code, software and/or
+# technologies under different licensing terms should contact us at
+# support@charmedlabs.com. 
+#
+
 from .about import __version__
 from .vizy import Vizy, dirs, VizyConfig, BASE_DIR, ETCDIR_NAME, APPSDIR_NAME, EXAMPLESDIR_NAME, SCRIPTSDIR_NAME
 from .vizyview import VizyView
--- src/vizy/wificonnection.py
+++ src/vizy/wificonnection.py
@@ -1,3 +1,13 @@
+#
+# This file is part of Vizy 
+#
+# All Vizy source code is provided under the terms of the
+# GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
+# Those wishing to use Vizy source code, software and/or
+# technologies under different licensing terms should contact us at
+# support@charmedlabs.com. 
+#
+
 import os
 import NetworkManager
 import uuid
