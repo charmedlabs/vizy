72d64be52ef352d01da71ade601958f84da5a4a8
rich legrand <rich@charmedlabs.com>
2022-04-30 18:37:43 -0500

    Clean up how we deal with intrinsics and video mode

--- examples/video/main.py
+++ examples/video/main.py
@@ -36,19 +36,22 @@ def line_y(x0, y0, x1, y1, y):
 
 class Perspective:
 
-    def __init__(self, camera, video, style=kritter.default_style, closed=True, shift=True, shear=True):
+    def __init__(self, camera, video, f, style=kritter.default_style, closed=True, shift=True, shear=True):
         self.camera = camera
         self.video = video
-        self._f = 248
+        self.grid = False
+        self.pixelsize = 1
+        self.crop_x = 1
+        self.crop_y = 1
+        self.f = f 
         self.shear_x = 0
         self.shear_y = 0
         self.reset()
 
-        self.more = not closed
         control_style = style
         style = style.copy()
         style['control_width'] = 0.1
-        enable = kritter.Kcheckbox(name="Perspective", value=self.more, style=style)
+        enable = kritter.Kcheckbox(name="Perspective", value=not closed, style=style)
         self.more_c = kritter.Kbutton(name=Kritter.icon("plus", padding=0), size="sm", disabled=closed)
         enable.append(self.more_c)
         grid = kritter.Kcheckbox(name="Show grid", value=False, style=style)
@@ -79,9 +82,9 @@ class Perspective:
         def func(is_open):
             return more_shear.out_name([Kritter.icon("plus"), "Shear"] if is_open else [Kritter.icon("minus"), "Shear"]) + [Output(collapse_shear.id, "is_open", not is_open)] 
 
-        @self.more_c.callback()
-        def func():
-            return self.set_more(not self.more)
+        @self.more_c.callback([State(self.collapse.id, "is_open")])
+        def func(is_open):
+            return self.set_more(not is_open)
 
         @enable.callback()
         def func(val):
@@ -146,15 +149,8 @@ class Perspective:
 
         @grid.callback()
         def func(value):
-            if value:
-                step = self.camera.resolution[0]//(GRID_DIVS+2)
-                for i in range(step//2, self.camera.resolution[0], step):
-                    self.video.draw_line(i, 0, i, self.camera.resolution[1], line={"color": f"rgba(0, 255, 0, 0.25)", "width": 2})
-                for i in range(step//2, self.camera.resolution[1], step):
-                    self.video.draw_line(0, i, self.camera.resolution[0], i, line={"color": f"rgba(0, 255, 0, 0.25)", "width": 2})
-            else:
-                self.video.draw_clear() 
-            return self.video.out_draw_overlay()
+            self.grid = value
+            return self.draw_grid()
 
     def reset(self):
         self._matrix = I_MATRIX
@@ -165,9 +161,18 @@ class Perspective:
         self.shift_x = 0
         self.shift_y = 0
 
+    def draw_grid(self):
+        self.video.draw_clear() 
+        if self.grid:
+            step = self.camera.resolution[0]//(GRID_DIVS+2)
+            for i in range(step//2, self.camera.resolution[0], step):
+                self.video.draw_line(i, 0, i, self.camera.resolution[1], line={"color": f"rgba(0, 255, 0, 0.25)", "width": 2})
+            for i in range(step//2, self.camera.resolution[1], step):
+                self.video.draw_line(0, i, self.camera.resolution[0], i, line={"color": f"rgba(0, 255, 0, 0.25)", "width": 2})
+        return self.video.out_draw_overlay()
+
     def set_more(self, val):
-            self.more = val
-            return self.more_c.out_name(Kritter.icon("minus", padding=0) if self.more else Kritter.icon("plus", padding=0)) + [Output(self.collapse.id, "is_open", self.more)]
+            return self.more_c.out_name(Kritter.icon("minus", padding=0) if val else Kritter.icon("plus", padding=0)) + [Output(self.collapse.id, "is_open", val)]
 
     def calc_roll(self):
         roll = self.roll*math.pi/180
@@ -189,22 +194,22 @@ class Perspective:
             x0 = 0
             x1 = self.camera.resolution[0]
         else:
-            vanish = center_x, self._f/math.tan(pitch) + center_y
+            vanish = center_x, self.f/math.tan(pitch) + center_y
             x0 = line_y(0, self.camera.resolution[1], vanish[0], vanish[1], 0)
             x1 = line_y(self.camera.resolution[0], self.camera.resolution[1], vanish[0], vanish[1], 0)
         if yaw==0:
             y0 = 0
             y1 = self.camera.resolution[1]
         else:
-            vanish = self._f/math.tan(yaw) + center_x, center_y
+            vanish = self.f/math.tan(yaw) + center_x, center_y
             y0 = line_x(self.camera.resolution[0], 0, vanish[0], vanish[1], 0)
             y1 = line_x(self.camera.resolution[0], self.camera.resolution[1], vanish[0], vanish[1], 0)
         p_in = np.float32([[0, y1], [self.camera.resolution[0], self.camera.resolution[1]], [x1, 0], [x0, y0]])
-        phi = math.atan(self.camera.resolution[1]/2/self._f)
+        phi = math.atan(self.camera.resolution[1]/2/self.f)
         y_stretch = math.sin(math.pi/2+phi)/math.sin(math.pi/2+pitch-phi)
         w = self.camera.resolution[0]/y_stretch
         x_offset = (self.camera.resolution[0] - w)/2
-        phi = math.atan(self.camera.resolution[0]/2/self._f)
+        phi = math.atan(self.camera.resolution[0]/2/self.f)
         x_stretch = math.sin(math.pi/2+phi)/math.sin(math.pi/2+yaw-phi)
         h = self.camera.resolution[1]/x_stretch
         y_offset = (self.camera.resolution[1] - h)/2
@@ -218,17 +223,26 @@ class Perspective:
     def matrix(self):
         return self._matrix
 
-    @property 
-    def f(self):
-        return self._f 
+    def set_intrinsics(self, f, shear_x, shear_y):
+        self.f = f 
+        self.shear_x = shear_x
+        self.shear_y = shear_y 
 
-    @f.setter
-    def f(self, value):
-        self._f = f 
-        self.calc_matrix() 
+    def set_video(self, pixelsize, crop_x, crop_y):
+        self.f *= self.pixelsize/pixelsize
+        self.shear_x *= crop_x/self.crop_x#/crop_x 
+        self.shear_y *= crop_y/self.crop_y#/crop_y 
+        print(self.f, self.shear_x, self.shear_y)
+        self.pixelsize = pixelsize 
+        self.crop_x = crop_x
+        self.crop_y = crop_y
+        self.calc_matrix()
+        return self.draw_grid()
 
     def transform(self, image):
         return image if np.array_equal(self._matrix, I_MATRIX) else cv2.warpPerspective(image, self._matrix, self.camera.resolution, flags=cv2.INTER_LINEAR)
+
+FOCAL_LENGTH = 2260 # measured in pixels
 
 class Video:
     def __init__(self):
@@ -236,13 +250,15 @@ class Video:
         self.camera = kritter.Camera(hflip=True, vflip=True)
         self.stream = self.camera.stream()
 
+        self.pixelsize_map = {'320x240x10bpp (cropped)': (2*1332/320, 1332/2028, 990/1520), '640x480x10bpp (cropped)': (2*1332/640, 1332/2028, 990/1520), '768x432x10bpp': (2*2028/768, 1, 1080/1520), '1280x720x10bpp': (2*2028/1280, 1, 1080/1520), '1280x960x10bpp (cropped)': (2*1332/1280, 1332/2028, 990/1520), '1920x1080x10bpp': (2*2028/1920, 1, 1080/1520), '2016x1520x10bpp': (2, 1, 1)}
+
         # Create Kritter server.
         kapp = Vizy()
         style = {"label_width": 3, "control_width": 6}
          # Create video component.
         self.video = kritter.Kvideo(width=self.camera.resolution[0], overlay=True)
         hist_enable = kritter.Kcheckbox(name='Histogram', value=False, style=style)
-        self.perspective = Perspective(self.camera, self.video, style=style)       
+        self.perspective = Perspective(self.camera, self.video, FOCAL_LENGTH, style=style)       
         mode = kritter.Kdropdown(name='Camera mode', options=self.camera.getmodes(), value=self.camera.mode, style=style)
         brightness = kritter.Kslider(name="Brightness", value=self.camera.brightness, mxs=(0, 100, 1), format=lambda val: '{}%'.format(val), style=style)
         framerate = kritter.Kslider(name="Framerate", value=self.camera.framerate, mxs=(self.camera.min_framerate, self.camera.max_framerate, 1), format=lambda val : '{} fps'.format(val), style=style)
@@ -255,6 +271,8 @@ class Video:
         awb_gains = dbc.Collapse([red_gain, blue_gain], id=kapp.new_id(), is_open=not self.camera.awb)     
         ir_filter = kritter.Kcheckbox(name='IR filter', value=kapp.power_board.ir_filter(), style=style)
         ir_light = kritter.Kcheckbox(name='IR light', value=kapp.power_board.vcc12(), style=style)
+
+        self.perspective.set_video(*self.pixelsize_map[self.camera.mode])
 
         @hist_enable.callback()
         def func(value):
@@ -272,7 +290,7 @@ class Video:
         @mode.callback()
         def func(value):
             self.camera.mode = value
-            return self.video.out_width(self.camera.resolution[0]) + framerate.out_value(self.camera.framerate) + framerate.out_min(self.camera.min_framerate) + framerate.out_max(self.camera.max_framerate)
+            return self.perspective.set_video(*self.pixelsize_map[self.camera.mode]) + self.video.out_width(self.camera.resolution[0]) + framerate.out_value(self.camera.framerate) + framerate.out_min(self.camera.min_framerate) + framerate.out_max(self.camera.max_framerate)
 
         @autoshutter.callback()
         def func(value):
