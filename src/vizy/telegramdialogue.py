ba1d84eb57945014d0def17d11672dac40647ede
Matt Peregoy <peregoywork@gmail.com>
2022-06-06 14:43:01 -0400

    piecemeal first draft display from timedialogue.py and gclouddialogue.py

--- src/vizy/telegramdialogue.py
+++ src/vizy/telegramdialogue.py
@@ -13,6 +13,7 @@ import time
 from datetime import datetime
 import base64
 import json
+from tkinter import dialog
 import cv2
 import dash_html_components as html
 import dash_core_components as dcc
@@ -20,7 +21,7 @@ from dash_devices import callback_context
 from kritter import Kritter, KtextBox, Ktext, Kdropdown, Kbutton, Kdialog, KokDialog, KsideMenuItem
 from dash_devices.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from kritter import Gcloud, Kritter
+from kritter import Kritter
 from .vizy import BASE_DIR
 
 NO_KEYS = 0
@@ -35,7 +36,24 @@ class TelegramDialog:
         self.kapp = kapp
         self.state = None
         self.bot_token_filename = os.path.join(self.kapp.etcdir, BOT_TOKEN_FILE)
-        self.gcloud = Gcloud(kapp.etcdir)
+        # self.gcloud = Gcloud(kapp.etcdir) # 
         
-        style = {"label_width": 3, "control_width": 6}
+        style = {"label_width": 3, "control_width": 6} # overall style..?
+
+        # copied from gclouddialog.py
+        self.error_text = Ktext(style={"control_width": 12})   
+        self.error_dialog = KokDialog(title=[Kritter.icon("exclamation-circle"), "Error"], layout=self.error_text)
+        self.success_text = Ktext(style={"control_width": 12})   
+        self.success_dialog = KokDialog(title=[Kritter.icon("check-square-o"), "Success"], layout=self.success_text)
+        # also copied from gclouddialogue.py
+        self.code = KtextBox(style={"control_width": 12}, placeholder="Paste bot token here.", service=None)
+        self.submit = Kbutton(name=[Kritter.icon("cloud-upload"), "Submit"], service=None)
+        self.code_dialog = Kdialog(title=[Kritter.icon("google"), "Submit code"], layout=self.code, left_footer=self.submit)
+
+        self.text = Ktext(name="Telegram", style={"label_width":1, "control_width": 1})
+
+        layout = [self.text, self.error_dialog, self.success_dialog, self.code_dialog]
+        dialog = Kdialog(title=[Kritter.icon("clock-o"), "Telegram"], layout=layout)
+        self.layout = KsideMenuItem("Telegram", dialog, "clock-o") # keeping clock-o for as temp icon 
+
 
