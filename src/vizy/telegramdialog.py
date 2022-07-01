621e83942943abb43ff06d88b0427c051534e791
richlegrand <rich@charmedlabs.com>
2022-07-01 15:49:58 -0500

    ...

--- src/vizy/telegramdialog.py
+++ src/vizy/telegramdialog.py
@@ -26,112 +26,79 @@ from kritter import Kritter, TelegramClient
 from .vizy import BASE_DIR
 
 
-"""
-Dialog development plan:
-Onboarding Dialog.  
-You can look at the code in VizyVisor like TimeDialog or GcloudDialog to see how dialogs are done.  
-1. needs to accept a token as input, save it to the etc directory as a json file (like GcloudDialog does).
-    a. activates 'has token' state 
-2. A button to test things 
-    a. needs to receive a message
-        i. bring up another dialog and wait for a message
-        ii. print the message once received
-    b. User dismisses by pressing OK. 
-"""
-
-"""
-Token States
-- change display depending on token state
-"""
-NO_TOKEN = 0
-HAS_TOKEN = 1
-
 class TelegramDialog:
 
     def __init__(self, kapp, pmask):
         self.kapp = kapp
-        self.token_state = None # state of token presence - has a token been successfully added or not
+        self.echo = False
         
         # Initialize Client and define callback_receive
         self.telegram_client = TelegramClient(self.kapp.etcdir)
         @self.telegram_client.callback_receive()
         def func(sender, message):
             print(f"Received: {message} from {sender}.")
-            self.telegram_client.text(sender, f'You said "{message}"')
-            # Test Image - url & local
-            # todo: inspect url image sending
-            self.telegram_client.image(sender, 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/300px-Gull_portrait_c')    
-            # # local images
-            # img1 = os.path.join(self.kapp.etcdir, 'test_1.jpeg')
-            # img2 = os.path.join(self.kapp.etcdir, 'test_2.jpeg')
-            # self.telegram_client.image(sender, img1)    
-            # self.telegram_client.image(sender, img2)
+            if self.echo:
+                self.kapp.push_mods(self.status.out_value(f'Received: "{message}"!') + self.echo_test.out_spinner_disp(False))
+                self.telegram_client.text(sender, f'You said "{message}".')
+                self.echo = False
 
-        # Styles
-        style = {"label_width": 6, "control_width": 6} # overall style..?
-        style2 = {"label_width": 2, "control_width": 6} # secondary style ?
-
-        # Main Dialog Title 
-        self.inner_title = Ktext(name="Telegram Client", style=style)
+        style = {"label_width": 2, "control_width": 6} 
         
-        # Token Submission 
-        self.token_text = KtextBox(name="Bot Token", placeholder="Paste Bot Token Here", style=style2)
-        self.token_submit_btn = Kbutton(name=[Kritter.icon('thumbs-up'), "Submit"])
-        self.token_text.append(self.token_submit_btn)
+        # Set token components
+        self.token_text = KtextBox(name="Token", placeholder="Paste Token Here", style=style)
+        self.submit_token = Kbutton(name=[Kritter.icon('thumbs-up'), "Submit"], spinner=True)
+        self.token_text.append(self.submit_token)
 
-        # Test Messages
-        self.test_message_text = KtextBox(name="Test Message", value="test message!", style=style2)
-        self.send_test_message_btn = Kbutton(name=[Kritter.icon("telegram"), "Send"])
-        self.test_message_text.append(self.send_test_message_btn)
-
-        # Remove Token 
+        # Remove, test components
         self.remove_token = Kbutton(name=[Kritter.icon("remove"), "Remove Token"])
+        self.echo_test = Kbutton(name=[Kritter.icon("comments"), "Echo test"], spinner=True)
+        self.remove_token.append(self.echo_test)
 
-        # Final Layout and Dialog Design  
-        layout = [self.inner_title, self.token_text, self.test_message_text, self.remove_token]
-        dialog = Kdialog(title=[Kritter.icon("telegram"), "Telegram Bot Configuration"], layout=layout)
-        #  vizy visor can remove display via this layout if user is not given permission
-        self.layout = KsideMenuItem("Telegram", dialog, "clock-o") # keeping clock-o for as temp icon 
+        # Display status
+        self.status = Ktext(style={"control_width": 12})
+
+        layout = [self.token_text, self.remove_token, self.status]
+        dialog = Kdialog(title=[Kritter.icon("commenting"), "Telegram Bot Configuration"], layout=layout)
+
+        self.layout = KsideMenuItem("Telegram", dialog, "commenting") 
         
         # Get and run state of dialog
         self.update_state() # set token for first time, ensuring proper display
 
         @dialog.callback_view()
         def func(open):
-            """Change appearance of dialog depending on Token State"""
             if open:
-                return self.update_state()
+                return self.update_state() # Entering -- update GUI state.
+            else:
+                self.echo = False # Leaving -- turn off echo if it's on.
 
-        @self.token_submit_btn.callback(self.token_text.state_value())
+        @self.submit_token.callback(self.token_text.state_value())
         def func(token):
-            '''pass in content of token_text, save locally to kapp.ectdir
-            ? encrypt after saving to kapp.etcdir inside client ?
-            ? save multiple ? 
-            '''
-            print(f'submited {token} as token')
-            self.telegram_client.set_token(token)
-            # self.token_state = HAS_TOKEN
-            return self.update_state()
-
-        @self.send_test_message_btn.callback(self.test_message_text.state_value())
-        def func(message):
-            print(f'{message}')
-            # self.telegram_client.text(sender, f"{message}")
-
+            self.kapp.push_mods(self.submit_token.out_spinner_disp(True))
+            mods = self.submit_token.out_spinner_disp(False) 
+            try:
+                self.telegram_client.set_token(token)
+            except Exception as e:
+                return mods + self.status.out_value(f"There has been an error: {e}")
+            return mods + self.update_state()
+                
         @self.remove_token.callback()
         def func():
-            print(f'remove click')
-            # if self.telegram_client.remove_token(): # success or failure boolean
-            #     self.token_state = NO_TOKEN            
-            self.telegram_client.remove_token()
+            try:
+                self.telegram_client.remove_token()
+            except Exception as e:
+                return self.status.out_value(f"There has been an error: {e}")
             return self.update_state()
         
-    # needs to be changed to work with telegram
-    def update_state(self):
-        # set token state depending if client has an assigned token or not - client.token should be either a token string or None object
-        self.token_state = HAS_TOKEN if self.telegram_client and self.telegram_client.token else NO_TOKEN
+        @self.echo_test.callback()
+        def func():
+            self.echo = True
+            return self.echo_test.out_spinner_disp(True) + self.status.out_value("Send a message to the Vizy Bot from your Telegram App...")
 
-        if self.token_state == HAS_TOKEN:
-            return self.token_text.out_disp(False) + self.token_submit_btn.out_disp(False) + self.test_message_text.out_disp(True) + self.send_test_message_btn.out_disp(True) + self.remove_token.out_disp(True)
-        elif self.token_state == NO_TOKEN:
-            return self.token_text.out_disp(True) + self.token_submit_btn.out_disp(True) + self.test_message_text.out_disp(False) + self.send_test_message_btn.out_disp(False) + self.remove_token.out_disp(False)
+    def update_state(self):
+        mods = self.echo_test.out_spinner_disp(False)
+        if self.telegram_client.running(): # Running is the same as having a token...
+            return mods + self.token_text.out_disp(False) + self.submit_token.out_disp(False) + self.remove_token.out_disp(True) + self.echo_test.out_disp(True) + self.status.out_value("Connected!")
+        else: # ... and not running, no token
+            return mods + self.token_text.out_disp(True) + self.submit_token.out_disp(True) + self.remove_token.out_disp(False) + self.echo_test.out_disp(False) + self.token_text.out_value("") + self.status.out_value("Enter Vizy Bot Token to connect.")
+    
