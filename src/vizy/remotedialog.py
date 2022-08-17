50d10c0dd46b5b4eaf6b3402c73053be709b61e4
rich legrand <rich@charmedlabs.com>
2022-08-16 19:46:38 -0500

    Added test and config buttons to textingdialog fixed issues with remotedialog's support for textvisor

--- src/vizy/remotedialog.py
+++ src/vizy/remotedialog.py
@@ -4,7 +4,7 @@
 # All Vizy source code is provided under the terms of the
 # GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
 # Those wishing to use Vizy source code, software and/or
-# technologies undbc.Collapseder different licensing terms should contact us at
+# technologies under different licensing terms should contact us at
 # support@charmedlabs.com. 
 #
 
@@ -12,12 +12,12 @@ import os
 import subprocess
 import json
 import datetime
-from threading import Thread
-from kritter.ktextvisor import KtextVisor, KtextVisorTable
+from threading import Thread, Condition
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
 from kritter import Kritter, ConfigFile, Ktext, Kbutton, Kdialog, KtextBox, Kcheckbox, KsideMenuItem
+from kritter.ktextvisor import KtextVisor, KtextVisorTable
 from dash_devices.dependencies import Input, Output, State
 
 CONFIG_FILE = "remote.json"
@@ -35,39 +35,31 @@ class RemoteDialog:
         self.kapp = kapp
         self.process = None
         self.run = False
-        self.address = ''
-        self.text_visor = tv
+        self.remote_address = None
+        self.cond = Condition()
 
         self.config_filename = os.path.join(self.kapp.etcdir, CONFIG_FILE)
         self.config = ConfigFile(self.config_filename, DEFAULT_CONFIG)
 
         self.key_filename = os.path.join(self.kapp.etcdir, KEY_FILE)
-        self.ssh_store()
-        self.stylize_page()
-        self.setup_callbacks()
-        self.text_visor_commands()
-
-    def ssh_store(self):
         # Generate ssh key even though we may not need it.  Once generated
         # we won't need to generate again.
         if not os.path.exists(self.key_filename):
             os.system(f'ssh-keygen -f {self.key_filename} -P ""')
         # Store key so we can copy.    
         self.url_store = dcc.Store(id=Kritter.new_id())
-        # If pub file exists, read and store key
         with open(self.key_filename+".pub") as f:
             self.key_store = dcc.Store(data=f.read(), id=Kritter.new_id())
 
-    def stylize_page(self):
         style = {"label_width": 4, "control_width": 8}
         self.custom_domain_c = Kcheckbox(name='Custom domain', value=self.config['subdomain']!="", style=style)
         self.subdomain_c = KtextBox(name="Subdomain", value=self.config['subdomain'], placeholder="Enter subdomain", style=style)
         localhost = Kbutton(name=[Kritter.icon("external-link"), "localhost.run"], href="https://admin.localhost.run", external_link=True, target="_blank")
-        self.copy_key = Kbutton(name=[Kritter.icon("copy"), "Copy key"], spinner=True)
-        localhost.append(self.copy_key)
+        copy_key = Kbutton(name=[Kritter.icon("copy"), "Copy key"], spinner=True)
+        localhost.append(copy_key)
         self.su_enable_c = Kcheckbox(name="Enable on start-up", value=self.config['start-up enable'], style=style)
         domain_card = dbc.Card([self.subdomain_c, self.su_enable_c, localhost])
-        self.domain_cont = dbc.Collapse(domain_card, id=self.kapp.new_id(), is_open=self.config['subdomain']!="", style=style)
+        self.domain_cont = dbc.Collapse(domain_card, id=kapp.new_id(), is_open=self.config['subdomain']!="", style=style)
 
         self.start_button = Kbutton(name=[Kritter.icon("play"), "Start"], spinner=True)
         # Use dbc button for copying URL because it can be rendered inline.
@@ -82,54 +74,6 @@ class RemoteDialog:
         if self.config['start-up enable'] and self.config['subdomain']:
             self.start_stop(True)
 
-
-
-
-    def start_stop(self, start):
-        # If no change necessary...
-        if self.run==start:
-            return 
-        # Start spinner before thread exits to avoid race condition.
-        self.kapp.push_mods(self.start_button.out_spinner_disp(True))
-        self.run = start
-        if start:
-            Thread(target=self.thread).start()
-        else:
-            if self.process:
-                try:
-                    self.process.terminate()
-                except:
-                    pass
-
-    
-    def thread(self):
-        # Create an ssh tunnel with localhost.run.  The StrictHostKeyChecking flag prevents 
-        # ssh from asking if you want to connect to an "unknown host" (unknown to it).  
-        if self.config['subdomain']!="":
-            command = ["ssh", "-i", self.key_filename, "-oStrictHostKeyChecking=no", "-R", f"{self.config['subdomain']}.{self.config['domain']}:80:localhost:80", "root@localhost.run", "--", "--output", "json"]
-        else:
-            command = ["ssh", "-oStrictHostKeyChecking=no", "-R", "80:localhost:80", "nokey@localhost.run", "--", "--output", "json"]
-        status = 'Press start to enable Web Sharing.'
-        while self.run:
-            self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-            while True:
-                out = self.process.stdout.readline()
-                if out==b"":
-                    break
-                out = json.loads(out.decode("utf-8"))
-                if out['status']=="success":
-                    self.kapp.push_mods(self.new_url(f"https://{out['address']}") + self.start_button.out_name([Kritter.icon("stop"), "Stop"]) + self.start_button.out_spinner_disp(False))
-                else:
-                    status = "Connection hasn't been established."
-                    self.run = False
-                    self.process.terminate()
-                    break
-
-            self.process.wait()
-            self.kapp.push_mods(self.start_button.out_name([Kritter.icon("play"), "Start"]) + self.start_button.out_spinner_disp(False) + self.status.out_value(status))
-
-
-    def setup_callbacks(self):
         @self.custom_domain_c.callback(self.subdomain_c.state_value())
         def func(val, domain):
             if val:
@@ -160,7 +104,7 @@ class RemoteDialog:
             function(click, url) {
                 var textArea = document.createElement("textarea");
                 textArea.value = url;
-                textArea.style.position = "fixed";  s
+                textArea.style.position = "fixed";  
                 document.body.appendChild(textArea);
                 textArea.focus();
                 textArea.select();
@@ -169,53 +113,85 @@ class RemoteDialog:
             }
         """
         self.kapp.clientside_callback(script, Output("_none", Kritter.new_id()), [Input(self.copy_url.id, "n_clicks")], state=[State(self.url_store.id, "data")])
-        self.kapp.clientside_callback(script, Output("_none", Kritter.new_id()), [Input(self.copy_key.id, "n_clicks")], state=[State(self.key_store.id, "data")])
+        self.kapp.clientside_callback(script, Output("_none", Kritter.new_id()), [Input(copy_key.id, "n_clicks")], state=[State(self.key_store.id, "data")])
 
-
-    def text_visor_commands(self):
         def remote(words, sender, context):
-            # start or get thread for remote tunnel
-            start()
-            status = 'no address yet' if self.address=='' else f"Go to [vizy link]({self.address}), Created at {datetime.datetime.now().strftime('%I:%M:%S %p')}"
-            return status
+            # If we're not running, start thread
+            if not self.run:
+                self.start_stop(True) 
+                # Wait for result
+                with self.cond:
+                    self.cond.wait()
 
-        def start(start=True):
-            self.run = start
-            if start:
-                Thread(target=thread).start()
-            else:
-                if self.process:
-                    try:
-                        self.process.terminate()
-                    except:
-                        pass
+            # Return result 
+            if self.remote_address:
+                return self.remote_address
+            else:                         
+                return "Web sharing wasn't able to start."
 
-        def thread():
-            # Create an ssh tunnel with localhost.run.  The StrictHostKeyChecking flag prevents 
-            # ssh from asking if you want to connect to an "unknown host" (unknown to it).  
-            # todo: make ternary
-            if self.config['subdomain']!="": command = ["ssh", "-i", self.key_filename, "-oStrictHostKeyChecking=no", "-R", f"{self.config['subdomain']}.{self.config['domain']}:80:localhost:80", "root@localhost.run", "--", "--output", "json"]
-            else: command = ["ssh", "-oStrictHostKeyChecking=no", "-R", "80:localhost:80", "nokey@localhost.run", "--", "--output", "json"]
-            while self.run:
-                self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-                while True:
-                    out = self.process.stdout.readline()
-                    if out==b"": break
-                    out = json.loads(out.decode("utf-8"))
-                    if out['status']=="success":
-                        self.address = f"{out['address']}" 
-                    else:
-                        self.run = False
-                        self.address = ''
-                        self.process.terminate()
-                self.process.wait()
-
-        # build and append commands to text visor
-        tv_table = KtextVisorTable({"remote": (remote, "Get SSH tunnel link to Vizy")})
-        @self.text_visor.callback_receive()
+        tv_table = KtextVisorTable({"webshare": (remote, "Get web share URL for Vizy.")})
+        @tv.callback_receive()
         def func(words, sender, context):
             return tv_table.lookup(words, sender, context)
 
+    def start_stop(self, start):
+        # If no change necessary...
+        if self.run==start:
+            return 
+        # Start spinner before thread exits to avoid race condition.
+        self.kapp.push_mods(self.start_button.out_spinner_disp(True))
+        self.run = start
+        if start:
+            Thread(target=self.thread).start()
+        else:
+            if self.process:
+                try:
+                    self.process.terminate()
+                except:
+                    pass
+
+    def new_url(self, url):
+        status = ['Go to ', html.A(url, href=url, target="_blank"), self.copy_url]
+        if self.config['subdomain']=="":
+            # Add time so we have an idea as to whether the prevous link expired on us. 
+            status.extend([html.Br(), f'(Created at {datetime.datetime.now().strftime("%I:%M:%S %p")})'])
+        return [Output(self.url_store.id, "data", url)] + self.status.out_value(status)
+
+
+    def thread(self):
+        # Create an ssh tunnel with localhost.run.  The StrictHostKeyChecking flag prevents 
+        # ssh from asking if you want to connect to an "unknown host" (unknown to it).  
+        if self.config['subdomain']!="":
+            command = ["ssh", "-i", self.key_filename, "-oStrictHostKeyChecking=no", "-R", f"{self.config['subdomain']}.{self.config['domain']}:80:localhost:80", "root@localhost.run", "--", "--output", "json"]
+        else:
+            command = ["ssh", "-oStrictHostKeyChecking=no", "-R", "80:localhost:80", "nokey@localhost.run", "--", "--output", "json"]
+        status = 'Press start to enable Web Sharing.'
+        while self.run:
+            self.remote_address = None
+            self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+            while True:
+                out = self.process.stdout.readline()
+                if out==b"":
+                    break
+                out = json.loads(out.decode("utf-8"))
+                if out['status']=="success":
+                    self.remote_address = f"https://{out['address']}"
+                    self.kapp.push_mods(self.new_url(self.remote_address) + self.start_button.out_name([Kritter.icon("stop"), "Stop"]) + self.start_button.out_spinner_disp(False))
+                else:
+                    try:
+                        status = f"Connection wasn't established: {out['message']}"
+                    except:
+                        status = f"Connection wasn't established."                        
+                    self.run = False
+                    self.process.terminate()
+                    self.remote_address = None
+                    break
+                with self.cond:
+                    self.cond.notify()
+
+            self.process.wait()
+            self.kapp.push_mods(self.start_button.out_name([Kritter.icon("play"), "Start"]) + self.start_button.out_spinner_disp(False) + self.status.out_value(status))
+        self.remote_address = None
 
     def close(self):
         self.start_stop(False)
--- src/vizy/textingdialog.py
+++ src/vizy/textingdialog.py
@@ -19,12 +19,11 @@ class TextingDialog:
         self.kapp = kapp
        
         # Initialize Client and define callback_receive
-        self.text_visor = tv 
-        self.texting_client = tv.text_client 
-        
+        self.text_visor = tv         
 
         style = {"label_width": 2, "control_width": 6} 
         
+        self.config = Kbutton(name=[Kritter.icon("external-link"), "Create token (BotFather)"], href="https://t.me/botfather", external_link=True, target="_blank") 
         # Set token components
         self.token_text = KtextBox(name="Token", placeholder="Paste Token Here", style=style)
         self.submit_token = Kbutton(name=[Kritter.icon('thumbs-up'), "Submit"], spinner=True)
@@ -32,31 +31,36 @@ class TextingDialog:
 
         # Remove, test components
         self.remove_token = Kbutton(name=[Kritter.icon("remove"), "Remove Token"])
+        self.test = Kbutton(name=[Kritter.icon("commenting"), "Send test text"])
+        self.remove_token.append(self.test)
 
         # Subscriber List, manageable list of message recipients
-        self.subscribers = self.text_visor.config['subscribers']
         self.subscriber_selection = ''
         self.delete_button = Kbutton(name=[Kritter.icon("trash"), "Delete"], disabled=True)
         self.delete_text = Ktext(style={"control_width": 12})
         self.delete_subscriber_yesno = KyesNoDialog(title="Delete Subscriber?", layout=self.delete_text, shared=True)
-        self.subscriber_select = Kdropdown(value=None, placeholder="Select subscriber...", options=self.subscribers.values())
+        self.subscriber_select = Kdropdown(value=None, placeholder="Select subscriber...")
         self.subscriber_select.append(self.delete_button) 
 
         # Display status
         self.status = Ktext(style={"control_width": 12})
 
-        layout = [self.token_text, self.remove_token, self.subscriber_select, self.delete_subscriber_yesno, self.status]
-        dialog = Kdialog(title=[Kritter.icon("commenting"), "Texting Bot Configuration"], layout=layout)
+        layout = [self.config, self.token_text, self.remove_token, self.subscriber_select, self.delete_subscriber_yesno, self.status]
+
+        dialog = Kdialog(title=[Kritter.icon("commenting"), "Texting Configuration"], layout=layout)
 
         self.layout = KsideMenuItem("Texting", dialog, "commenting") 
         
-        # Get and run state of dialog
-        self.update_state() # set token for first time, ensuring proper display
-
         @dialog.callback_view()
         def func(open):
             if open:
                 return self.update_state() # Entering -- update GUI state.
+
+        # text_visor will call us when a user subscribes or unsubscribes, so we can update
+        # the GUI accordingly.
+        @self.text_visor.callback_subscribe()
+        def func():
+            self.kapp.push_mods(self.update_state()) # update GUI state.
 
         @self.submit_token.callback(self.token_text.state_value())
         def func(token):
@@ -65,7 +69,7 @@ class TextingDialog:
             self.kapp.push_mods(self.submit_token.out_spinner_disp(True))
             mods = self.submit_token.out_spinner_disp(False) 
             try:
-                self.texting_client.set_token(token)
+                self.text_visor.text_client.set_token(token)
             except Exception as e:
                 return mods + self.status.out_value(f"There has been an error: {e}")
             return mods + self.update_state()
@@ -75,7 +79,7 @@ class TextingDialog:
             if not callback_context.client.authentication&pmask:
                 return
             try:
-                self.texting_client.remove_token()
+                self.text_visor.text_client.remove_token()
             except Exception as e:
                 return self.status.out_value(f"There has been an error: {e}")
             return self.update_state()
@@ -88,28 +92,32 @@ class TextingDialog:
 
         @self.delete_button.callback()
         def func():
-            return self.delete_text.out_value(f'Are you sure you want to delete "{self.subscriber_selection}" subscriber from list?') + self.delete_subscriber_yesno.out_open(True)
+            return self.delete_text.out_value(f'Are you sure you want to delete "{self.subscriber_selection}" subscriber?') + self.delete_subscriber_yesno.out_open(True)
+
+        @self.test.callback()
+        def func():
+            self.text_visor.send("This is a test. Thank you for your cooperation.")
 
         @self.delete_subscriber_yesno.callback_response()
         def func(val):
             # remove subscriber from recipient list where user's name is key
             if val:
                 # find id associated with username
-                userid = [id for (id, name) in self.subscribers.items() if name == self.subscriber_selection]
+                userid = [id_ for id_, subscriber in self.text_visor.config['subscribers'].items() if subscriber['name']==self.subscriber_selection]
                 userid = userid[0]              # unwrap id
-                del self.subscribers[userid]    # delete key from subscribers
-                self.text_visor.config['subscribers'] = self.subscribers    # update subscriber list
+                del self.text_visor.config['subscribers'][userid]    # delete key from subscribers
                 self.text_visor.config.save()                               # save list to file
                 self.kapp.push_mods(self.subscriber_select.out_value(''))   # clear output
                 return self.update_state()
 
+
     def update_state(self):
         # update subscriber list
-        self.subscribers = self.text_visor.config['subscribers']
-        if self.texting_client.running(): # Running is the same as having a token...
-            return self.token_text.out_disp(False) + self.submit_token.out_disp(False) + self.remove_token.out_disp(True) + self.status.out_value("Connected!") + self.subscriber_select.out_disp(True) + self.delete_button.out_disp(True) + self.subscriber_select.out_options(self.subscribers.values())
+        if self.text_visor.text_client.running(): # Running is the same as having a token...
+            subscribers = [subscriber['name'] for subscriber in self.text_visor.config['subscribers'].values()]
+            return self.token_text.out_disp(False) + self.submit_token.out_disp(False) + self.remove_token.out_disp(True) + self.status.out_value("Connected!") + self.subscriber_select.out_disp(True) + self.delete_button.out_disp(True) + self.subscriber_select.out_options(subscribers) + self.test.out_disp(bool(self.text_visor.config['subscribers'])) + self.config.out_disp(False)
         else: # ... and not running, no token
-            return self.token_text.out_disp(True) + self.submit_token.out_disp(True) + self.remove_token.out_disp(False) + self.token_text.out_value("") + self.status.out_value("Enter Vizy Bot Token to connect.") + self.subscriber_select.out_disp(False) + self.delete_button.out_disp(False) 
+            return self.token_text.out_disp(True) + self.submit_token.out_disp(True) + self.remove_token.out_disp(False) + self.token_text.out_value("") + self.status.out_value("Enter Vizy Bot Token to connect.") + self.subscriber_select.out_disp(False) + self.delete_button.out_disp(False) + self.test.out_disp(False) + self.config.out_disp(True)
   
     def close(self):
         self.text_visor.close()  
