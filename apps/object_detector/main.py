d441d898704f4d94a904a343bdde773e69c94031
rich legrand <rich@charmedlabs.com>
2022-11-04 12:45:41 -0500

    Add _delete_remote to FileMirror

--- apps/object_detector/main.py
+++ apps/object_detector/main.py
@@ -93,6 +93,7 @@ class FileMirror:
             # set marker to 0 mtime (12/31/1969)
             self._update_marker(0)
         self.thread_ = Thread(target=self.thread)
+        self.check_delete = True
         self.run_thread = True
         self.thread_.start()
 
@@ -107,11 +108,21 @@ class FileMirror:
         while self.run_thread:
             # Find all images in path, sort by mtime
             files = os.listdir(self.src_path)
+            files.remove(MARKER) # MARKER doesn't count as a legit file
+            local_files = set(files)
+            # Detect removal of local files and handle deletions remotely
+            check_delete = self.check_delete or self.local_files.difference(local_files)
+            self.local_files = local_files
+            if check_delete:
+                self._delete_remote()
+                self.check_delete = False
+
             files.sort(key=lambda f : os.path.getmtime(os.path.join(self.src_path, f)))
             mtime = os.path.getmtime(self.marker)
             # Get rid of all files that are less than marker mtime.
             # (These have already been uploaded.)
-            files = [f for f in files if os.path.getmtime(os.path.join(self.src_path, f))>=mtime and f!=MARKER]
+            files = [f for f in files if os.path.getmtime(os.path.join(self.src_path, f))>=mtime]
+
             error = False
             for file in files:
                 if not self.run_thread:
@@ -137,6 +148,23 @@ class FileMirror:
                 self._update_marker(mtime)
             else: # We're either waiting for new data or we've errored -- don't thrash.
                 sleep(1)
+
+    def _delete_remote(self):
+        try:
+            remote_files = set(self.file_client.list(self.dst_path))
+        except Exception as e:
+            print("Unable to get remote files", e)
+            return
+        diff = remote_files.difference(self.local_files)
+        for file in diff:
+            if not self.run_thread:
+                return
+            print("Deleting", file)
+            try:
+                self.file_client.delete(os.path.join(self.dst_path, file))
+                print("done")
+            except Exception as e:
+                print("Unable to remove remote file", file, e)
 
     def _get_filename(self, ext):
         return os.path.join(self.src_path, kritter.time_stamped_file(ext))
