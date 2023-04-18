46041be98c5d62619be1ecf24150f1da63e26d4a
rich legrand <rich@charmedlabs.com>
2023-04-18 17:29:51 -0500

    Add speed overlay, timestamps

--- apps/radar/main.py
+++ apps/radar/main.py
@@ -10,6 +10,7 @@
 
 import os
 from threading import Thread
+import datetime
 import kritter
 import cv2
 import numpy as np
@@ -19,23 +20,33 @@ import dash_html_components as html
 from vizy import Vizy, MediaDisplayQueue
 from kritter.ktextvisor import KtextVisor, KtextVisorTable, Image
 import time
+from PIL import Image, ImageDraw, ImageFont
 
 NOISE_FLOOR = 25*3
 BINS = 100
-COLUMN_THRESHOLD = 25
-STATE_NONE = 0
-STATE_OCCLUDED = 1
-STATE_FULL = 2  
-DATA_TIMEOUT = 20 
+BIN_THRESHOLD = 25
+DATA_TIMEOUT = 20 # seconds
+SPEED_DISPLAY_TIMEOUT = 3 # seconds
 CAMERA_WIDTH = 768
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
 MEDIA_DIR = os.path.join(BASEDIR, "media")
 CONFIG_FILE = "radar.json"
+DEFAULT_CALIBRATION = 0.33 # MPH*seconds/bins
+KM_PER_MILE = 1.60934
+FONT_SIZE = 60 
+FONT_COLOR = (0, 255, 0)
+STATE_NONE = 0
+STATE_OCCLUDED = 1
+STATE_FULL = 2  
 
 DEFAULT_CONFIG = {
     "brightness": 50,
     "detection_sensitivity": 75,
+    "kph": False, 
     "gphoto_upload": False,
+    "left_pointing": False, 
+    "left_calibration": None, # left moving, MPH
+    "right_calibration": None # right moving, MPH
 }
 
 class Video: 
@@ -44,13 +55,13 @@ class Video:
         self.kapp = Vizy()
         config_filename = os.path.join(self.kapp.etcdir, CONFIG_FILE)      
         self.config = kritter.ConfigFile(config_filename, DEFAULT_CONFIG)               
-        
+        self.font = ImageFont.truetype(os.path.join(BASEDIR, "font.ttf"), FONT_SIZE)        
         if not os.path.isdir(MEDIA_DIR):
             os.makedirs(MEDIA_DIR)
         # Create and start camera.
         self.camera = kritter.Camera(hflip=True, vflip=True)
         self.stream = self.camera.stream(False)
-        self.stream.load("/home/pi/vizy/etc/motionscope/car3/video.raw")
+        self.stream.load("/home/pi/vizy/etc/motionscope/car1/video.raw")
         self.pointing_right = True 
         
         style = {"label_width": 3, "control_width": 6}
@@ -73,36 +84,69 @@ class Video:
         # Run Kritter server, which blocks.
         self.kapp.run()
         self.run_grab = False
+        
+    def _timestamp(self):
+        return datetime.datetime.now().strftime("%D %H:%M:%S")
 
-    def handle_end(self, data, pic):
+    def handle_end(self, data, pic, left):
         if len(data[0]):
+            # Take all of the data and fit to a line.  This will give us the most likely speed given noise, 
+            # that is, erroneous data is drown out by valid data. 
             A = np.vstack([data[1], np.ones(len(data[1]))]).T
             speed, _ = np.linalg.lstsq(A, data[0], rcond=None)[0]
             speed = abs(speed)
-            print(speed)
-            
-            filename = os.path.join(MEDIA_DIR, kritter.time_stamped_file("jpg"))
-            data = {"speed": speed, "width": pic.shape[1], "height": pic.shape[0]}
-            cv2.imwrite(filename, pic)
+            # Choose calibration based on direction of travel
+            if left:
+                calibration = self.config["left_calibration"] or self.config["right_calibration"] or DEFAULT_CALIBRATION
+            else: # moving right
+                calibration = self.config["right_calibration"] or self.config["left_calibration"] or DEFAULT_CALIBRATION
+            speed *= calibration
+            if self.config["kph"]:
+                speed *= KM_PER_MILE
+                
+            filename_ = kritter.time_stamped_file("jpg")
+            filename = os.path.join(MEDIA_DIR, filename_)
+            data = {"speed": speed, "left_moving": left, "left_pointing": self.config["left_pointing"], "timestamp": self._timestamp(), "width": pic.shape[1], "height": pic.shape[0]}
+            cv2.imwrite(filename+"_", pic) # write image without speed overlay
+            img = Image.fromarray(pic, "RGB")
+            drw = ImageDraw.Draw(img)
+            drw.text((0, 0), f'{round(speed)} {"kph" if self.config["kph"] else "mph"}', fill=FONT_COLOR, font=self.font)
+            pic = np.asarray(img)
+            cv2.imwrite(filename, pic) # write image with speed overlay
             kritter.save_metadata(filename, data)
             self.kapp.push_mods(self.media_queue.out_images())
+            return speed
 
     # Frame grabbing thread
-    def grab(self):        
+    def grab(self):  
+        speed_disp = None
         frame0 = None
         cols = None
         left_state = right_state = STATE_NONE
         left_pic = right_pic = None
-        hist_cols = np.arange(0, BINS, dtype='uint') 
+        hist_cols = np.arange(0, BINS, dtype='uint')
+        last_timestamp = None
         while self.run_grab:
+            mods = []
             # Get frame
             frame_orig = self.stream.frame()
             if frame_orig is None:
                 self.stream.seek(0)
                 frame0 = None
+                # left_state, left_pic is motion to left
+                # right_state, right_pic is motion to right
                 left_state = right_state = STATE_NONE
                 left_pic = right_pic = None
+                speed_disp = None
                 continue
+
+            timestamp = self._timestamp()
+            if timestamp!=last_timestamp:
+                self.video.overlay.draw_clear()
+                self.video.overlay.draw_text(0, frame_orig[0].shape[0]-1, timestamp, fillcolor="black", font=dict(family="sans-serif", size=12, color="white"), xanchor="left", yanchor="bottom")
+                mods += self.video.overlay.out_draw()
+                last_timestamp = timestamp
+
             
             frame = frame_orig[0]
             if cols is None:
@@ -114,19 +158,29 @@ class Video:
                 # Take diffence of all 3 color channels
                 for i in range(3):
                     diff += cv2.absdiff(frame[i], frame0[i])
-                # Send frame
-                #diff = np.clip(diff, 0, 255)
+                # Try to get rid of noise by thresholding
                 th = diff>NOISE_FLOOR
-                #print(th.sum())
+                # Take the thresholded pixels and associate with column values
                 th = cols[th]
+                # Create a "histogram of motion".  We're essentially taking the image and dividing it into BINS 
+                # number of columns.
                 hist = np.histogram(th, BINS, (0, diff.shape[1]-1))
-                col_thresh = hist_cols[hist[0]>COLUMN_THRESHOLD]
+                # Further threshold the columns to eliminate columns that don't have "significant data".
+                col_thresh = hist_cols[hist[0]>BIN_THRESHOLD]
+                # The rest of the code will look at the first (leftmost) column of motion (col_thresh[0]) and the 
+                # last (rightmost) column of motion (col_threshj[1]).  
+                # If we see motion of col_thresh[0], start recording data in to right_data (right-moving object data).
+                # Stop recording when we see motion on col_thresh[BINS-1]
+                # If we see motion of col_thresh[BINS-1], start recording data in to left_data (left-moving object data).
+                # Stop recording when we see motion on col_thresh[0]
                 if len(col_thresh):
                     if col_thresh[-1]==BINS-1:
                         if right_state==STATE_FULL:
                             if right_pic is None:
                                 right_pic = frame_prev[0].copy()
-                            self.handle_end(right_data, right_pic)
+                            speed = self.handle_end(right_data, right_pic, False)
+                            if speed: 
+                                speed_disp = speed, time.time()
                             right_state = left_state = STATE_NONE
                             left_pic = right_pic = None
                         elif left_state==STATE_NONE:
@@ -137,7 +191,10 @@ class Video:
                         if left_state==STATE_FULL:
                             if left_pic is None:
                                 left_pic = frame_prev[0].copy()
-                            self.handle_end(left_data, left_pic)
+                            speed = self.handle_end(left_data, left_pic, True)
+                            if speed:
+                                speed = speed
+                                speed_disp = speed, time.time()
                             left_state = right_state = STATE_NONE
                             left_pic = right_pic = None
                         elif right_state==STATE_NONE:
@@ -148,26 +205,36 @@ class Video:
                         right_pic = frame_prev[0].copy()
 
                     if left_state:
+                        # Add column data
                         left_data[0] = np.append(left_data[0], col_thresh[0])
+                        # Add timestamp data
                         left_data[1] = np.append(left_data[1], frame_orig[1])
                         t = left_data[1][-1] -left_data[1][0]
                         if t>DATA_TIMEOUT:
                             left_state = STATE_NONE
                     if right_state:
+                        # Add column data
                         right_data[0] = np.append(right_data[0], col_thresh[-1])
+                        # Add timestamp data
                         right_data[1] = np.append(right_data[1], frame_orig[1])
                         t = right_data[1][-1] -right_data[1][0]
                         if t>DATA_TIMEOUT:
                             right_data = STATE_NONE
                         
-
-                #avg = np.average(th)
-                #print(avg)
-                #diff = np.where(diff>NOISE_FLOOR, 255, 0)
-                #diff = diff.astype('uint8')
-                self.video.push_frame(frame_orig)
+                if speed_disp is None:
+                    self.video.push_frame(frame_orig)
+                else: # overlay speed ontop of video 
+                    img = Image.fromarray(frame_orig[0], "RGB")
+                    drw = ImageDraw.Draw(img)
+                    drw.text((0, 0), f'{round(speed_disp[0])} {"kph" if self.config["kph"] else "mph"}', fill=FONT_COLOR, font=self.font)
+                    speed_frame = np.asarray(img)
+                    self.video.push_frame(speed_frame)
+                    if time.time()-speed_disp[1]>SPEED_DISPLAY_TIMEOUT:
+                        speed_disp = None 
+                        
             frame0 = frame.copy()
             frame_prev = frame_orig
-
+            self.kapp.push_mods(mods)
+            
 if __name__ == "__main__":
     Video()
