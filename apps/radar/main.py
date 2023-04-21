e10ce769c46e77b1884664b16710becb74580527
rich legrand <rich@charmedlabs.com>
2023-04-21 13:55:08 -0500

    Fix various bugs for radar app

--- apps/radar/main.py
+++ apps/radar/main.py
@@ -27,8 +27,7 @@ import plotly.graph_objs as go
 
 NOISE_FLOOR = 25*3
 BINS = 100
-BIN_THRESHOLD = 50
-DATA_TIMEOUT = 20 # seconds
+DATA_TIMEOUT = 10 # seconds
 SPEED_DISPLAY_TIMEOUT = 3 # seconds
 CAMERA_WIDTH = 768
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
@@ -41,10 +40,12 @@ FONT_COLOR = (0, 255, 0)
 STATE_NONE = 0
 STATE_OCCLUDED = 1
 STATE_FULL = 2  
+MINIMUM_DATA = 5
+SHUTTER_SPEED = 0.001
 
 DEFAULT_CONFIG = {
     "brightness": 50,
-    "detection_sensitivity": 75,
+    "sensitivity": 50,
     "kph": False, 
     "gphoto_upload": False,
     "left_pointing": False, 
@@ -63,16 +64,20 @@ class Video:
             os.makedirs(MEDIA_DIR)
         # Create and start camera.
         self.camera = kritter.Camera(hflip=True, vflip=True)
-        self.stream = self.camera.stream(False)
-        self.stream.load("/home/pi/vizy/etc/motionscope/car3/video.raw")
+        self.camera.autoshutter = False
+        self.camera.shutter_speed = SHUTTER_SPEED
+        self.stream = self.camera.stream() #(False)
+        #self.stream.load("/home/pi/vizy/etc/motionscope/car3/video.raw")
         self.pointing_right = True 
-        
-        style = {"label_width": 3, "control_width": 6}
 
-        # Create video component and histogram enable.
+        self.sensitivity_range = kritter.Range((1, 100), (200, 25), inval=self.config['sensitivity']) 
+        self.bin_threshold = self.sensitivity_range.outval
+
+        style = {"label_width": 2, "control_width": 4}
         self.video = kritter.Kvideo(width=self.camera.resolution[0], overlay=True)
         self.media_queue = MediaDisplayQueue(MEDIA_DIR, CAMERA_WIDTH, CAMERA_WIDTH) 
-        self.brightness = kritter.Kslider(name="Brightness", value=self.camera.brightness, mxs=(0, 100, 1), format=lambda val: f'{val}%', style={"control_width": 4}, grid=False)
+        self.brightness = kritter.Kslider(name="Brightness", value=self.camera.brightness, mxs=(0, 100, 1), format=lambda val: f'{val}%', style=style)
+        self.sensitivity = kritter.Kslider(name="Sensitivity", value=self.config['sensitivity'], mxs=(1, 100, 1), format=lambda val: f'{int(val)}%', style=style)
         self.gcloud = kritter.Gcloud(self.kapp.etcdir)
         self.gphoto_interface = self.gcloud.get_interface("KstoreMedia")
         self.store_media = kritter.SaveMediaQueue(path=MEDIA_DIR)
@@ -91,12 +96,23 @@ class Video:
         )
 
         self.graph = dcc.Graph(style={"display": "none"}, id=self.kapp.new_id())
-        self.kapp.layout = html.Div([self.video, self.media_queue.layout, self.brightness, self.graph, self.calib_dialog], style={"padding": "15px"})
+        self.kapp.layout = html.Div([self.video, self.media_queue.layout, self.brightness, self.sensitivity, self.graph, self.calib_dialog], style={"padding": "15px"})
 
+        self.kapp.push_mods(self.media_queue.out_images())
+        
         @self.brightness.callback()
         def func(value):
             self.config['brightness'] = value
             self.camera.brightness = value
+            self.config.save()
+
+        @self.sensitivity.callback()
+        def func(value):
+            self.config['sensitivity'] = value
+            self.sensitivity_range.inval = value
+            print(self.bin_threshold)
+            self.bin_threshold = self.sensitivity_range.outval
+            print(value, self.bin_threshold)
             self.config.save()
 
         @self.media_queue.dialog_image_callback()
@@ -149,33 +165,38 @@ class Video:
     
     def handle_end(self, data, pic, left):
         data_y, data_time = data
-        if len(data_y): # We can get false triggers with 0 data.
-            # Take all of the data and fit to a line.  This will give us the most likely speed given noise, 
-            # that is, erroneous data is drown out by valid data. 
-            A = np.vstack([data_time, np.ones(len(data_time))]).T
-            m, b = np.linalg.lstsq(A, data_y, rcond=None)[0]
-            speed_raw = abs(m)
-            # Choose calibration based on direction of travel
-            if left:
-                calibration = self.config["left_calibration"] or self.config["right_calibration"] or DEFAULT_CALIBRATION
-            else: # moving right
-                calibration = self.config["right_calibration"] or self.config["left_calibration"] or DEFAULT_CALIBRATION
-            speed = speed_raw*calibration
-            if self.config["kph"]:
-                speed *= KM_PER_MILE
-                
-            filename_ = kritter.time_stamped_file("jpg")
-            filename = os.path.join(MEDIA_DIR, filename_)
-            metadata = {"speed": speed, "speed_raw": speed_raw, "left_moving": left, "left_pointing": self.config["left_pointing"], "timestamp": self._timestamp(), "data": [list(data_time), list(data_y)], "width": pic.shape[1], "height": pic.shape[0]}
-            cv2.imwrite(filename+"_", pic) # write image without speed overlay
-            pic = self._overlay_speed(pic, speed)
-            cv2.imwrite(filename, pic) # write image with speed overlay
-            kritter.save_metadata(filename, metadata)
-            self.kapp.push_mods(self.media_queue.out_images())
-            figure = go.Figure(data=[go.Scatter(x=data_time, y=data_y, mode='lines+markers'), go.Scatter(x=[data_time[0], data_time[-1]], y=[m*data_time[0]+b, m*data_time[-1]+b])], layout=self.graph_layout)
-            #self.graph = dcc.Graph(figure=figure)
-            self.kapp.push_mods([Output(self.graph.id, "style", {"display": "block"}), Output(self.graph.id, "figure", figure)])
-            return speed
+        # deal with minimum data and left motion that looks like it's going right and right motion that looks like it's going left
+        if len(data_y)<MINIMUM_DATA or (left and data_y[0]-data_y[-1]<0) or (not left and data_y[0]-data_y[-1]>0):
+            return None
+        # Take all of the data and fit to a line.  This will give us the most likely speed given noise, 
+        # that is, erroneous data is drown out by valid data. 
+        A = np.vstack([data_time, np.ones(len(data_time))]).T
+        result = np.linalg.lstsq(A, data_y, rcond=None)
+        m, b = result[0]
+        residual = result[1]
+        print("residual", residual)
+        speed_raw = abs(m)
+        # Choose calibration based on direction of travel
+        if left:
+            calibration = self.config["left_calibration"] or self.config["right_calibration"] or DEFAULT_CALIBRATION
+        else: # moving right
+            calibration = self.config["right_calibration"] or self.config["left_calibration"] or DEFAULT_CALIBRATION
+        speed = speed_raw*calibration
+        if self.config["kph"]:
+            speed *= KM_PER_MILE
+            
+        filename_ = kritter.time_stamped_file("jpg")
+        filename = os.path.join(MEDIA_DIR, filename_)
+        metadata = {"speed": speed, "speed_raw": speed_raw, "left_moving": left, "left_pointing": self.config["left_pointing"], "timestamp": self._timestamp(), "data": [list(data_time), list(data_y)], "width": pic.shape[1], "height": pic.shape[0]}
+        cv2.imwrite(filename+"_", pic) # write image without speed overlay
+        pic = self._overlay_speed(pic, speed)
+        cv2.imwrite(filename, pic) # write image with speed overlay
+        kritter.save_metadata(filename, metadata)
+        self.kapp.push_mods(self.media_queue.out_images())
+        figure = go.Figure(data=[go.Scatter(x=data_time, y=data_y, mode='lines+markers'), go.Scatter(x=[data_time[0], data_time[-1]], y=[m*data_time[0]+b, m*data_time[-1]+b])], layout=self.graph_layout)
+        #self.graph = dcc.Graph(figure=figure)
+        self.kapp.push_mods([Output(self.graph.id, "style", {"display": "block"}), Output(self.graph.id, "figure", figure)])
+        return speed
 
     # Frame grabbing thread
     def grab(self):  
@@ -190,7 +211,7 @@ class Video:
             mods = []
             # Get frame
             frame_orig = self.stream.frame()
-            if frame_orig is None:
+            if 0: #frame_orig is None:
                 self.stream.seek(0)
                 frame0 = None
                 # left_state, left_pic is motion to left
@@ -214,73 +235,77 @@ class Video:
                 cols = np.atleast_2d(r).repeat(repeats=frame.shape[0], axis=0)
             frame = cv2.split(frame)
             if frame0:
-                diff = 0
-                # Take diffence of all 3 color channels
-                for i in range(3):
-                    diff += cv2.absdiff(frame[i], frame0[i])
-                # Try to get rid of noise by thresholding
-                th = diff>NOISE_FLOOR
-                # Take the thresholded pixels and associate with column values
-                th = cols[th]
-                # Create a "histogram of motion".  We're essentially taking the image and dividing it into BINS 
-                # number of columns.
-                hist = np.histogram(th, BINS, (0, diff.shape[1]-1))
-                # Further threshold the columns to eliminate columns that don't have "significant data".
-                col_thresh = hist_cols[hist[0]>BIN_THRESHOLD]
-                # The rest of the code will look at the first (leftmost) column of motion (col_thresh[0]) and the 
-                # last (rightmost) column of motion (col_threshj[1]).  
-                # If we see motion of col_thresh[0], start recording data in to right_data (right-moving object data).
-                # Stop recording when we see motion on col_thresh[BINS-1]
-                # If we see motion of col_thresh[BINS-1], start recording data in to left_data (left-moving object data).
-                # Stop recording when we see motion on col_thresh[0]
-                if len(col_thresh):
-                    if col_thresh[-1]==BINS-1:
-                        if right_state==STATE_FULL:
-                            if right_pic is None:
-                                right_pic = frame_prev[0].copy()
-                            speed = self.handle_end(right_data, right_pic, False)
-                            if speed: 
-                                speed_disp = speed, time.time()
-                            right_state = left_state = STATE_NONE
-                            left_pic = right_pic = None
-                        elif left_state==STATE_NONE:
-                            left_state = STATE_FULL 
-                            left_data = [np.array([]), np.array([])]
+                try:
+                    diff = 0
+                    # Take diffence of all 3 color channels
+                    for i in range(3):
+                        diff += cv2.absdiff(frame[i], frame0[i])
+                    # Try to get rid of noise by thresholding
+                    th = diff>NOISE_FLOOR
+                    # Take the thresholded pixels and associate with column values
+                    th = cols[th]
+                    # Create a "histogram of motion".  We're essentially taking the image and dividing it into BINS 
+                    # number of columns.
+                    hist = np.histogram(th, BINS, (0, diff.shape[1]-1))
+                    # Further threshold the columns to eliminate columns that don't have "significant data".
+                    col_thresh = hist_cols[hist[0]>self.bin_threshold]
+                    # The rest of the code will look at the first (leftmost) column of motion (col_thresh[0]) and the 
+                    # last (rightmost) column of motion (col_threshj[1]).  
+                    # If we see motion of col_thresh[0], start recording data in to right_data (right-moving object data).
+                    # Stop recording when we see motion on col_thresh[BINS-1]
+                    # If we see motion of col_thresh[BINS-1], start recording data in to left_data (left-moving object data).
+                    # Stop recording when we see motion on col_thresh[0]
+                    if len(col_thresh):
+                        if col_thresh[-1]==BINS-1:
+                            if right_state==STATE_FULL:
+                                if right_pic is None:
+                                    right_pic = frame_prev[0].copy()
+                                speed = self.handle_end(right_data, right_pic, False)
+                                if speed: 
+                                    speed_disp = speed, time.time()
+                                right_state = left_state = STATE_NONE
+                                left_pic = right_pic = None
+                            elif left_state==STATE_NONE:
+                                left_state = STATE_FULL 
+                                left_data = [np.array([]), np.array([])]
 
-                    if col_thresh[0]==0:
-                        if left_state==STATE_FULL:
-                            if left_pic is None:
-                                left_pic = frame_prev[0].copy()
-                            speed = self.handle_end(left_data, left_pic, True)
-                            if speed:
-                                speed = speed
-                                speed_disp = speed, time.time()
-                            left_state = right_state = STATE_NONE
-                            left_pic = right_pic = None
-                        elif right_state==STATE_NONE:
-                            right_state = STATE_OCCLUDED
-                            right_data = [np.array([]), np.array([])]
-                    elif right_state==STATE_OCCLUDED:
-                        right_state = STATE_FULL
-                        right_pic = frame_prev[0].copy()
-
-                    if left_state:
-                        # Add column data
-                        left_data[0] = np.append(left_data[0], col_thresh[0])
-                        # Add timestamp data
-                        left_data[1] = np.append(left_data[1], frame_orig[1])
-                        t = left_data[1][-1] -left_data[1][0]
-                        if t>DATA_TIMEOUT:
-                            left_state = STATE_NONE
-                    if right_state:
-                        # Add column data
-                        right_data[0] = np.append(right_data[0], col_thresh[-1])
-                        # Add timestamp data
-                        right_data[1] = np.append(right_data[1], frame_orig[1])
-                        t = right_data[1][-1] -right_data[1][0]
-                        if t>DATA_TIMEOUT:
-                            right_data = STATE_NONE
-                        
+                        if col_thresh[0]==0:
+                            if left_state==STATE_FULL:
+                                if left_pic is None:
+                                    left_pic = frame_prev[0].copy()
+                                speed = self.handle_end(left_data, left_pic, True)
+                                if speed:
+                                    speed_disp = speed, time.time()
+                                left_state = right_state = STATE_NONE
+                                left_pic = right_pic = None
+                            elif right_state==STATE_NONE:
+                                right_state = STATE_OCCLUDED
+                                right_data = [np.array([]), np.array([])]
+                        elif right_state==STATE_OCCLUDED:
+                            right_state = STATE_FULL
+                            right_pic = frame_prev[0].copy()
+    
+                        if left_state:
+                            # Add column data
+                            left_data[0] = np.append(left_data[0], col_thresh[0])
+                            # Add timestamp data
+                            left_data[1] = np.append(left_data[1], frame_orig[1])
+                            t = left_data[1][-1] - left_data[1][0]
+                            if t>DATA_TIMEOUT:
+                                left_state = STATE_NONE
+                            
+                        if right_state:
+                            # Add column data
+                            right_data[0] = np.append(right_data[0], col_thresh[-1])
+                            # Add timestamp data
+                            right_data[1] = np.append(right_data[1], frame_orig[1])
+                            t = right_data[1][-1] - right_data[1][0]
+                            if t>DATA_TIMEOUT:
+                                right_state = STATE_NONE
+                except Exception as e:
+                    print("***", e)
+                    breakpoint()
+                    
                 if speed_disp is None:
                     self.video.push_frame(frame_orig)
                 else: # overlay speed ontop of video 
